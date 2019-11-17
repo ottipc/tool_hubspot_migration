997335ebed10d16e8db28f045deeccf3ff46e07d
obraun <obraun@ssystems.de>
2019-11-17 18:36:35 +0100

    ADD finishing csv importer and adding excpetion handling

--- rest_client.py
+++ rest_client.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import sys
 
 class RestClient:
 
@@ -42,6 +42,7 @@ class RestClient:
         #student.gpa = float(4.0)
         return client
      def create_contact(self, row):
+      try:
         print("try t persist contact in hubspot : {}".format(row[7]))
         endpoint = "https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/{}/?hapikey=730c761f-9db5-41c3-a391-b981552855bf".format(row[7])
         headers = {}
@@ -51,15 +52,23 @@ class RestClient:
   "properties": [
     {
       "property": "email",
-      "value": "test@test.de"
+      "value": "%s" % row[7]
     },
     {
+      "property": "Login",
+      "value": "%s" % row[0]
+    },
+    #{
+    #  "property": "Mitglied-Status",
+    #  "value": " %s " % row[1]
+    #},
+    {
       "property": "firstname",
-      "value": " %s " % row[2]
+      "value": "%s" % row[2]
     },
     {
       "property": "lastname",
-      "value": " %s " % row[3]
+      "value": "%s" % row[3]
     },
     {
       "property": "website",
@@ -71,25 +80,93 @@ class RestClient:
     },
     {
       "property": "phone",
-      "value": " %s " % row[3]
+      "value": "%s" % row[3]
+    },
+    {
+      "property": "mobilephone",
+      "value": "%s" % row[5]
+    },
+    {
+      "property": "mg_nr",
+      "value": "%s" % row[8]
     },
     {
       "property": "address",
-      "value": " %s " % row[11]
+      "value": "%s" % row[11]
+    },
+    {
+      "property": "date_of_birth",
+      "value": "%s" % row[12]
+    },
+    {
+      "property": "Mehrwertsteuer",
+      "value": "%s" % row[13]
+    },
+    {
+      "property": "Kontoinhaber",
+      "value": "%s" % row[14]
+    },
+    {
+      "property": "Kontonummer",
+      "value": "%s" % row[15]
+    },
+    {
+      "property": "Bankleitzahl",
+      "value": "%s" % row[16]
+    },
+    {
+      "property": "name_der_bank",
+      "value": "%s" % row[17]
+    },
+    {
+      "property": "IBAN",
+      "value": "%s" % row[18]
+    },
+    {
+      "property": "BIC",
+      "value": "%s" % row[19]
+    },
+    {
+      "property": "sepa_mandat_vom",
+      "value": "%s" % row[20]
+    },
+    {
+      "property": "sepa_referenz",
+      "value": "%s" % row[21]
     },
     {
       "property": "city",
-      "value": " %s " % row[10]
+      "value": "%s" % row[10]
+    },
+    {
+      "property": "fax",
+      "value": "%s" % row[6]
     },
     {
       "property": "state",
       "value": "MA"
     },
     {
+      "property": "datum_der_letzten_Buchung",
+      "value": "%s" % row[22]
+    },
+    {
+      "property": "wert_der_letzten_Buchung",
+      "value": "%s" % row[23]
+    },
+    {
       "property": "zip",
-      "value": " %s " % row[9]
+      "value": "%s" % row[9]
+    },
+    {
+      "property": "kontaktkategorie",
+      "value": "%s" % row[24]
     }
   ]
-})  
+})      
+        print("Setting Call...")
         r = requests.post( url = endpoint, data = data, headers = headers )
         print(r.text)
+      except Exception as e:
+         #e = sys.exc_info()[0]
+        print( "<p>Error: %s</p>" % str(e) )
