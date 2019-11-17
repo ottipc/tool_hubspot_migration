65ef6778322a8600cf32636a49150e3a21fab00b
obraun <obraun@ssystems.de>
2019-11-17 21:04:39 +0100

    ADD config and change testdata directory

---
+++ config.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+appconfig = {'csvdirectory': '/home/codi/Entwicklung/tool_hubspot_api/csvfiles/',
+         'api_url': 'https://api.hubapi.com/contacts/v1/',
+         'api_key': '730c761f-9db5-41c3-a391-b981552855bf'}
