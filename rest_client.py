e8e3f98f7f48a6538d59c0bbac247e85e563e417
obraun <obraun@ssystems.de>
2019-11-17 21:05:08 +0100

    ADD add config in code

--- rest_client.py
+++ rest_client.py
@@ -1,33 +1,13 @@
+#!/usr/bin/env python
 import requests
 import json
 import sys
-
+import os
+import config as cfg
 class RestClient:
 
-     columnLoginName = "Login" #0
-     columnMitgliedstatusName = "Mitglied-Status" #1
-     columnVornameName = "Vorname" #2
-     columnFamiliennameName = "Familienname" #3
-     columnTelefonName = "Telefon" #4
-     columnMobilName = "Mobil" #5
-     columnFaxName = "Fax" #6
-     columnEmailName = "E-Mail" #7
-     columnMgNrName = "Mg.Nr" #8
-     columnPLZName = "PLZ" #9
-     columnOrtName = "Ort" #10
-     columnAnschriftName = "Anschrift" #11
-     columnGeburtstagName = "Geburtstag" #12
-     columnMehrwertsteuerName = "Mehrwertsteuer" #13
-     columnKontoinhaberName = "Kontoinhaber" #14
-     columnKontonummerName = "Kontonummer" #15
-     columnBankleitzahlName = "Bankleitzahl" #16
-     columnBankName = "Name der Bank" #17
-     columnIBANName = "IBAN" #18
-     columnBICName = " BIC" #19
-     columnSEPAMName = "SEPA Mandat vom" #20
-     columnSEPARName = "SEPA Referenz" #21
-     url = "https://api.hubapi.com/contacts/v1/"
-     API_KEY = '730c761f-9db5-41c3-a391-b981552855bf'
+     #url = "https://api.hubapi.com/contacts/v1/"
+     #API_KEY = '730c761f-9db5-41c3-a391-b981552855bf'
     # Initializer / Instance Attributes
      def __init__(self, url, API_KEY):
         self.url = url
@@ -43,8 +23,9 @@ class RestClient:
         return client
      def create_contact(self, row):
       try:
-        print("try t persist contact in hubspot : {}".format(row[7]))
-        endpoint = "https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/{}/?hapikey=730c761f-9db5-41c3-a391-b981552855bf".format(row[7])
+        print("try to persist contact in hubspot : {}".format(row[7]))
+        endpoint = "{}contact/createOrUpdate/email/{}/?hapikey={}".format(cfg.appconfig['api_url'], row[7], cfg.appconfig['api_key'])
+        print("Endpoint : {}".format(endpoint))
         headers = {}
         headers["Content-Type"]="application/json"
         
@@ -167,6 +148,29 @@ class RestClient:
         print("Setting Call...")
         r = requests.post( url = endpoint, data = data, headers = headers )
         print(r.text)
+        #self.sendMail()
       except Exception as e:
          #e = sys.exc_info()[0]
         print( "<p>Error: %s</p>" % str(e) )
+        
+        
+     def findCSV(self):
+        file_names = []
+        for file in os.listdir("./csvfiles"):
+            if file.endswith(".csv"):
+                file_names.append(os.path.join("./csvfiles", file))
+                print(os.path.join("./csvfiles", file))
+        return file_names
+        
+     def sendMail(self):
+        sendmail_location = "/usr/sbin/sendmail" # sendmail location
+        p = os.popen("%s -t" % sendmail_location, "w")
+        p.write("From: %s\n" % "from@somewhere.com")
+        p.write("To: %s\n" % "otti@petitcode.com")
+        p.write("Subject: thesubject\n")
+        p.write("\n") # blank line separating headers from body
+        p.write("body of the mail")
+        status = p.close()
+        if status != 0:
+              print("Sendmail exit status", status)
+   
