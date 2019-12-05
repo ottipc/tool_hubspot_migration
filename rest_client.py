43d86cc18ab1fa716353943b1f1d7f5e88b0d1e7
obraun <obraun@ssystems.de>
2019-12-05 23:05:31 +0100

    MOD formatted and email feature

--- rest_client.py
+++ rest_client.py
@@ -4,11 +4,10 @@ import json
 import sys
 import os
 import config as cfg
+from datetime import datetime, time
+import calendar
 class RestClient:
 
-     #url = "https://api.hubapi.com/contacts/v1/"
-     #API_KEY = '730c761f-9db5-41c3-a391-b981552855bf'
-    # Initializer / Instance Attributes
      def __init__(self, url, API_KEY):
         self.url = url
         self.key = API_KEY
@@ -17,15 +16,9 @@ class RestClient:
         client = RestClient(url, API_KEY)
         client.url = url
         client.key = API_KEY
-        #student.major = major
-        # Note: I didn't need to create a variable in the class definition before doing this.
-        #student.gpa = float(4.0)
         return client
      
      def get_code(self):
-        #client = RestClient(url, API_KEY)
-        #client.url = url
-        #client.key = API_KEY
         print('Get Code ...')
         
         endpoint = "https://app.hubspot.com/oauth/authorize"
@@ -34,17 +27,11 @@ class RestClient:
         r = requests.get( url = endpoint, data = data, headers = headers )
         print('Response Get Code .....{}'.format(r.text))
         print(r.text)
-        #student.major = major
-        # Note: I didn't need to create a variable in the class definition before doing this.
-        #student.gpa = float(4.0)
         return r
      
      
      
      def refresh_token(self):
-        #client = RestClient(url, API_KEY)
-        #client.url = url
-        #client.key = API_KEY
         print('Refresh Token ...')
         
         endpoint = "{}token".format(cfg.appconfig['api_oauth_url'])
@@ -60,9 +47,6 @@ class RestClient:
      
      
      def get_token(self):
-        #client = RestClient(url, API_KEY)
-        #client.url = url
-        #client.key = API_KEY
         print('Get Token ...')
         
         endpoint = "{}token".format(cfg.appconfig['api_oauth_url'])
@@ -72,10 +56,6 @@ class RestClient:
         r = requests.post( url = endpoint, data = data, headers = headers )
         print('Response .....{}'.format(r.text))
         print(r.text)
-        
-        #student.major = major
-        # Note: I didn't need to create a variable in the class definition before doing this.
-        #student.gpa = float(4.0)
         return r
      
      
@@ -86,22 +66,24 @@ class RestClient:
             raise Exception("Email is empty, not able to persist in Hubspot")
     
         print("try to persist contact in hubspot : %s" % row[7])
-        endpoint = "{}contact/createOrUpdate/email/{}".format(cfg.appconfig['api_url'], row[7])
+        endpoint = "{}contact/createOrUpdate/email/{}".format(cfg.appconfig['api_url'], row[7].strip())
         #print("Endpoint : {}".format(endpoint))
         headers = {}
         headers["Content-Type"]="application/json"
         headers["Authorization"]="Bearer {}".format(access_token)
-        
+        dateofbirth = self.convertToTimestamp(row[12], "%d.%m.%Y")
+        sepaMandatFrom = self.convertToTimestamp(row[20],"%d.%m.%Y")
+        dateOfLastBooking = self.convertToTimestamp(row[22],"%d-%m-%Y")
         data = json.dumps({
   "properties": [
     {
       "property": "email",
-      "value": "%s" % row[7]
+      "value": "%s" % row[7].strip()
     },
-    {
-      "property": "Login",
-      "value": "%s" % row[0]
-    },
+    #{
+    #  "property": "Login",
+    #  "value": "%s" % row[0]
+    #},
     #{
     #  "property": "Mitglied-Status",
     #  "value": " %s " % row[1]
@@ -130,54 +112,54 @@ class RestClient:
       "property": "mobilephone",
       "value": "%s" % row[5]
     },
-    {
-      "property": "mg_nr",
-      "value": "%s" % row[8]
-    },
+    #{
+    #  "property": "mg_nr",
+    #  "value": "%s" % row[8]
+    #},
     {
       "property": "address",
       "value": "%s" % row[11]
     },
     {
       "property": "date_of_birth",
-      "value": "%s" % row[12]
+      "value": "%s" % dateofbirth
     },
-    {
-      "property": "Mehrwertsteuer",
-      "value": "%s" % row[13]
-    },
-    {
-      "property": "Kontoinhaber",
-      "value": "%s" % row[14]
-    },
-    {
-      "property": "Kontonummer",
-      "value": "%s" % row[15]
-    },
-    {
-      "property": "Bankleitzahl",
-      "value": "%s" % row[16]
-    },
-    {
-      "property": "name_der_bank",
-      "value": "%s" % row[17]
-    },
-    {
-      "property": "IBAN",
-      "value": "%s" % row[18]
-    },
-    {
-      "property": "BIC",
-      "value": "%s" % row[19]
-    },
-    {
-      "property": "sepa_mandat_vom",
-      "value": "%s" % row[20]
-    },
-    {
-      "property": "sepa_referenz",
-      "value": "%s" % row[21]
-    },
+    #{
+    #  "property": "Mehrwertsteuer",
+    #  "value": "%s" % row[13]
+    #},
+    #{
+    #  "property": "Kontoinhaber",
+    #  "value": "%s" % row[14]
+    #},
+    #{
+    #  "property": "Kontonummer",
+    #  "value": "%s" % row[15]
+    #},
+    #{
+    #  "property": "Bankleitzahl",
+    #  "value": "%s" % row[16]
+    #},
+    #{
+    #  "property": "name_der_bank",
+    #  "value": "%s" % row[17]
+    #},
+    #{
+    #  "property": "IBAN",
+    #  "value": "%s" % row[18]
+    #},
+    #{
+    #  "property": "BIC",
+    #  "value": "%s" % row[19]
+    #},
+    #{
+    #  "property": "sepa_mandat_vom",
+    #  "value": "%s" % sepaMandatFrom
+    #},
+    #{
+    #  "property": "sepa_referenz",
+    #  "value": "%s" % row[21]
+    #},
     {
       "property": "city",
       "value": "%s" % row[10]
@@ -192,7 +174,7 @@ class RestClient:
     },
     {
       "property": "datum_der_letzten_Buchung",
-      "value": "%s" % row[22]
+      "value": "%s" % dateOfLastBooking
     },
     {
       "property": "wert_der_letzten_Buchung",
@@ -211,11 +193,12 @@ class RestClient:
         #print("Setting Call...")
         r = requests.post( url = endpoint, data = data, headers = headers )
         #print(r.text)
-        print(r.status_code)
+        #print(r.status_code)
         if r.status_code==404:
             raise Exception("Api URL not available : " . endpoint)
         if r.status_code!=200:
             raise Exception(r.text)
+        return row[7]
       except Exception as e:
         raise e
         
@@ -228,27 +211,50 @@ class RestClient:
                 print(os.path.join(csvdir, file))
         return file_names
         
-     def sendMailCorrect(self, csvfile):
+     def sendMailCorrect(self, csvfile, data):
         sendmail_location = "/usr/sbin/sendmail" # sendmail location
         p = os.popen("%s -t" % sendmail_location, "w")
         p.write("From: %s\n" % cfg.appconfig['email_from'])
         p.write("To: %s\n" % cfg.appconfig['email_to'])
+        p.write("CC: %s\n" % ",".join(cfg.appconfig['email_cc']))
         p.write("Subject: CSV imported correctly\n")
+        p.write("\n") # blank line separating headers from body
+        p.write("Processed:")
+        p.write("\n") # blank line separating headers from body
+        for st in data:
+            p.write(st)
+            p.write("\n") # blank line separating headers from body
         p.write("\n") # blank line separating headers from body
         p.write("File imported correctly to Hubspot and moved to processed: {} ".format(csvfile))
         status = p.close()
         if status != 0:
               print("Sendmail exit status", status)
               
-     def sendMailError(self, csvfile, etext):
+     def sendMailError(self, csvfile, etext, data):
         sendmail_location = "/usr/sbin/sendmail" # sendmail location
         p = os.popen("%s -t" % sendmail_location, "w")
         p.write("From: %s\n" % cfg.appconfig['email_from'])
         p.write("To: %s\n" % cfg.appconfig['email_to'])
+        p.write("CC: %s\n" % ",".join(cfg.appconfig['email_cc']))
         p.write("Subject: ERROR importing CSV !\n")
         p.write("\n") # blank line separating headers from body
+        p.write("Processed:")
+        p.write("\n") # blank line separating headers from body
+        for st in data:
+            p.write(st)
+            p.write("\n") # blank line separating headers from body
         p.write("\nFile NOT imported correctly to Hubspot and moved to erroprocessed: {} ".format(csvfile))
         p.write("\n\nError: {}".format(etext))
         status = p.close()
         if status != 0:
               print("Sendmail exit status", status)
+              
+              
+     def convertToTimestamp(self,dateAsStr, pattern):
+        if not dateAsStr:
+              return dateAsStr
+        if dateAsStr == '..':
+              return ''
+        midnightDate =  datetime.combine(datetime.strptime(dateAsStr, pattern), datetime.min.time())
+        dateutc = calendar.timegm(midnightDate.utctimetuple())*1000 
+        return dateutc
