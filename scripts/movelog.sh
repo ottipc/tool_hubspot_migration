434b28bcb69bd4e0bf0e3f034ef44a783eb81d2c
obraun <obraun@ssystems.de>
2019-12-08 02:29:41 +0100

    MOD adjust movelog script

--- scripts/movelog.sh
+++ scripts/movelog.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 now=`date +"%m_%d_%Y:%M_S"`
-cp /home/codi/Entwicklung/tool_hubspot_api/log/call_hubspot_api.log /home/codi/Entwicklung/tool_hubspot_api/log/call_hubspot_api.$now.log
+mv /var/www/vhosts/mcarena/tool_hubspot_migration/log/crontab_hubspot_api.log /var/www/vhosts/mcarena/tool_hubspot_migration/log/crontab_hubspot_api.$now.log
+mv /var/www/vhosts/mcarena/tool_hubspot_migration//log/call_hubspot_api.log /var/www/vhosts/mcarena/tool_hubspot_migration//log/call_hubspot_api.$now.log
